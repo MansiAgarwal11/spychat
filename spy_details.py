34b4249e6d904df4a5f929734343602bc3810670
MansiAgarwal11 <r18522mansi@dpsrkp.net>
2017-10-03 19:55:44 +0530

    classes used

--- spy_details.py
+++ spy_details.py
@@ -1,5 +1,17 @@
-spy = {'name': "James Bond",
-'salutation': "Mr.",
-'age': 23,
-'rating': 4.7,
-'is_online':True}
+
+class Spy:
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = None
+
+
+class ChatMessage:
+    def __init__(self, message, time, sent_by_me):
+        self.message = message
+        self.time = time
+        self.sent_by_me = sent_by_me
